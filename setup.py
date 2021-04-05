7e1948e36abffe660f54f5157993e722ef19b63c
Jason Gomes <JasonG7234@gmail.com>
2021-04-04 23:38:19 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@ author_email = 'ribeiro11075@gmail.com'
 author = 'David Ribeiro'
 requires_python = '>=3.6.0'
 version = '0.0.0'
-install_requires = ['sys', 'time', 'datetime', 're', 'requests', 'pandas', 'selenium', 'bs4']
 extras_require = {}
 include_package_data = True
 exclude = ['config']
